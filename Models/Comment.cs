6680660b9eef2ba0ba52ded8dc081ef3bc52a1d6
Palitu Ana <anarosca66@gmail.com>
2023-02-11 11:56:04 +0200

    Add project files.

---
+++ Models/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace MyBlog.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int ArticleId { get; set; }
+        public string Author { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
